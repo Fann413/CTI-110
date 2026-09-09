142ad3a5cb363cd95631baa7e164f7a68f0c63b5
Fann413 <jfannunc43@gmail.com>
2026-09-09 11:22:25 -0400

    Add files via upload

---
+++ P1LAB1_FannJacob.py
@@ -0,0 +1,12 @@
+# Jacob Fann
+# 9/9/26
+# P1LAB1
+# Display a welcome message to user
+
+# Input from user, get first/last name
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+
+# Output
+
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110.")
